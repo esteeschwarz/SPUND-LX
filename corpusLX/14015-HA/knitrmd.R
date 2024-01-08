bb6ae0139cf42d7fa6ed15a34c2aabb071f2d377
esteeschwarz <91368979+esteeschwarz@users.noreply.github.com>
2024-01-08 13:29:25 +0100

    upd

--- corpusLX/14015-HA/knitrmd.R
+++ corpusLX/14015-HA/knitrmd.R
@@ -3,5 +3,4 @@ library(knitr)
 #?render_markdown()
 #?knit
 #?output_format
-knit("treadme.Rmd",output="README.md")#,output_format(knitr = knitr_options(opts_chunk = list(dev = 'png')),
-                                       #pandoc = pandoc_options(to = "html"),keep_md = T))
+knit("README.Rmd",output="README.md")
