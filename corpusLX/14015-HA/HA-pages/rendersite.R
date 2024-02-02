c12c2ec8f154664a3acc4185f8877059c936574f
estee <91368979+esteeschwarz@users.noreply.github.com>
2024-02-02 15:18:42 +0100

    no menu yet

--- corpusLX/14015-HA/HA-pages/rendersite.R
+++ corpusLX/14015-HA/HA-pages/rendersite.R
@@ -1,6 +1,9 @@
 .libPaths(new = "/home/runner/work/SPUND-LX/SPUND-LX/rlibs", include.site = TRUE)
 library(rmarkdown)
 #library(bookdown)
+#library(knitr)
 
-render_site(input = "./corpusLX/14015-HA/HA-pages")
+
+setwd("corpusLX/14015-HA/HA-pages")
+render_site()
 #render_site(input = "./")
