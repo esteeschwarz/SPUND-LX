65de76e60d60ff67b8e7d5ac29b6370777ba0daf
esteeschwarz <91368979+esteeschwarz@users.noreply.github.com>
2024-06-17 11:41:34 +0200

    upd adjectives with referent

--- DA/hoecke-voigt/duell_pos-tag.R
+++ DA/hoecke-voigt/duell_pos-tag.R
@@ -29,3 +29,124 @@ adj.l<-readLines("~/Documents/GitHub/SPUND-LX/DA/hoecke-voigt/adjectives.txt")
 adj.regex<-paste0(adj.l,"|")
 adj.regex.c<-paste(adj.regex,collapse = "")
 write_clip(adj.regex.c)
+###
+# get nouns related to adjectives
+# function from 14015-HA:
+x<-t3
+x2<-as.list(t3)
+get.corpus.deprel<-function(x){
+#  x1<-data.frame(x)
+ # x<-x1
+  # assign unique id to token
+  x$sbc.token.id<-paste0(x$sbc.id,".",  1:length(x$sbc.id))  
+#  x$sbc.token.id<-1:length(x$sbc.id)  
+  x$pos.0<-"lfd.pos"  
+  x$obj<-NA
+  tdf<-t(x)
+  rtdf<-rownames(tdf)
+  rtdf.0<-which(rtdf=="pos.0")
+  all.zero<-which(tdf=="lfd.pos")-rtdf.0
+  #  all.zero.0<-all.zero-length(rtdf)
+  # pos.deprel<-length(rtdf)-grep("dep",rtdf)
+  # pos.head<-length(rtdf)-grep("head",rtdf)
+  # pos.upos<-length(rtdf)-grep("upos",rtdf)
+  # pos.lemma<-length(rtdf)-grep("lemma",rtdf)
+  # pos.token<-length(rtdf)-which(rtdf=="token")
+  # pos.token.id<-length(rtdf)-which(rtdf=="token_id")
+  #########################################
+  pos.deprel<-all.zero+grep("dep",rtdf)
+  pos.head<-all.zero+grep("head",rtdf)
+  pos.upos<-all.zero+grep("upos",rtdf)
+  pos.lemma<-all.zero+grep("lemma",rtdf)
+  pos.token<-all.zero+which(rtdf=="token")
+  pos.token.id<-all.zero+which(rtdf=="token_id")
+  #########################
+  # t.head<-all.zero-pos.head
+  # t.head<-all.zero-grep("head",rtdf)
+  # t.id<-all.zero-pos.token.id
+  # t.tag<-all.zero-pos.upos
+  # t.token<-all.zero-pos.token
+  # t.lemma<-all.zero-pos.lemma
+  # t.deprel<-all.zero-pos.deprel
+  t.head<-pos.head
+  t.id<-pos.token.id
+  t.tag<-pos.upos
+  t.token<-pos.token
+  t.lemma<-pos.lemma
+  t.deprel<-pos.deprel
+  m.h.t.0<-which(tdf[t.head]==0)
+  h.t.pos.rel<-as.double(tdf[t.id])-as.double(tdf[t.head])
+  h.t.pos.rel[m.h.t.0]<-0
+  h.t.pos.abs<-t.id-(h.t.pos.rel*length(tdf[,1]))
+  #tdf[h.t.pos.abs]
+  h.t.value<-t.token-(h.t.pos.rel*length(tdf[,1]))
+  
+  #head(tdf[h.t.value])
+  m1<-which(h.t.value<0)
+  sum(m1)
+  h.t.value[m1]<-1 # prevent negative subscripts
+  h.l.value<-t.lemma-(h.t.pos.rel*length(tdf[,1]))
+  m2<-which(h.l.value<0)
+  sum(m2)
+  h.l.value[m2]<-1 # prevent negative subscripts
+  head(tdf[h.l.value])
+  ####################
+  all.obj<-tdf=="obj"
+  #all.obj[]
+  all.ob.w<-which(all.obj)
+  all.ob.w
+  obj.head<-as.double(all.ob.w-1)
+  tdf[obj.head]
+  obj.tag<-all.ob.w-4 #4 
+  tdf[obj.tag]
+  obj.lemma<-all.ob.w-5 #5
+  tdf[obj.lemma]
+  obj.token<-all.ob.w-6 #6
+  tdf[obj.token]
+  obj.id<-as.double(all.ob.w-7) #7
+  tdf[obj.id]
+  h.t.o.pos.rel<-as.double(tdf[obj.id])-as.double(tdf[obj.head])
+  h.t.o.pos.abs<-obj.id-(h.t.o.pos.rel*length(tdf[,1]))
+  tdf[h.t.o.pos.abs]
+  h.t.o.value<-obj.token-(h.t.o.pos.rel*length(tdf[,1]))
+  tdf[h.t.o.value]
+  h.l.o.value<-obj.lemma-(h.t.o.pos.rel*length(tdf[,1]))
+  tdf[h.l.o.value]
+  obj.pos<-all.ob.w+5
+  tdf[obj.pos]<-tdf[h.l.o.value]
+  ####################
+  #obj.df<-get.lemma.obj(x)
+  #tdf[obj.df$obj.pos]<-as.character(obj.df$obj)
+  #tdf[16,1]
+  # tdf[675]
+  tdf.r<-as.data.frame(t(tdf))
+  m<-!is.na(tdf.r$obj)
+  sum(m)
+  #length(tdf[obj.pos])
+  #length(tdf[h.t.value])
+  x$obj<-tdf.r$obj
+  x$head_token_value<-tdf[h.t.value]
+  x$head_lemma_value<-tdf[h.l.value]
+  # mode(x$line)<-"double"
+  # mode(x$sbc.id)<-"double"
+  # mode(x$token_id)<-"double"
+  # mode(x$head_token_id)<-"double"
+  
+  return(x)  
+  
+}
+
+# t.ns<-colnames(t3)
+# t.ns
+# colnames(t3)[1]<-"sbc.id"
+# corpus.head.list<-lapply(scb.pos.df.list, get.corpus.deprel)
+# 
+# corpus.head.list<-lapply(x2, get.corpus.deprel)
+# x2$sbc.id
+t4<-get.corpus.deprel(t3)
+adj.ref<-t4$head_token_value[m.adj]
+adj.list<-t4$token[m.adj]
+m.adj[is.na(m.adj)]<-F
+sum(m.adj)
+adj.df<-data.frame(tok.id=t4$sbc.token.id[m.adj], adj=adj.list,ref=adj.ref)
+write.csv(adj.df,"~/Documents/GitHub/SPUND-LX/DA/hoecke-voigt/adjectives.df.csv")
