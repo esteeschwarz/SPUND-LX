35efd2ec9edf48da6d0c4b63ac4a2dfe3b9c75c2
estee <91368979+esteeschwarz@users.noreply.github.com>
2024-12-27 16:01:14 +0100

    upd, stuck doc structure

--- intLX/corpus-cwb-annotate.R
+++ intLX/corpus-cwb-annotate.R
@@ -8,44 +8,50 @@ library(udpipe)
 model<-udpipe::udpipe_load_model(paste(wd,"../corpuslx/german-gsd-ud-2.5-191206.udpipe",sep = "/"))
 ##################################
 ## test
-#x<-com.df[3,]
-#clean<-cleancomments
+x<-com.df[1,]
+clean<-cleancomments
 ##
-get.ann.df_ann<-function(x,clean){
+chunk
+get.ann.df_ann<-function(x,clean,chunk){
   t.out<-clean
   comment<-gsub(t.out," ",x$comment)
   cor.tok<-udpipe::udpipe_annotate(model,comment)
   df<-as.data.frame(cor.tok)
   df.se<-df[1,]
   df.se[1,]<-""
-  df.se$token<-"<s>"
+  # df.se$token<-"<s>"
+   df.se$token<-s.tag
   df.s.end<-df.se
   df.s.end$token<-'</s>'
   sent.u<-unique(df$sentence_id)
   sent.u<-sent.u[!is.na(sent.u)]
   sent.u
-  df.e<-rbind(df.se,df[df$sentence_id==sent.u[1],],df.s.end)
-  for(s in sent.u[2:length(sent.u)]){
+  df.ex<-df[df$sentence_id==sent.u[1],]
+  s.tag<-paste0('<s timestamp="',x$timestamp,'" sent_id ="',paste0(chunk,'.',x$comment_id),'.',x$sentence_id,'" author="',x$author,'" url="',x$url,'" url_id="',chunk,'" date="',x$date,'">')
+  s.tag
+  df.e<-rbind(df.se,,df.s.end)
+#  for(s in sent.u[2:length(sent.u)]){
     df.es<-df[df$sentence_id==s,]
     if(length(sent.u)>1)
       df.e<-rbind(df.e,df.se,df.es,df.s.end)
-  }
+ # }
   df<-df.e
-  df$timestamp<-x$timestamp
-  df$com_id<-x$comment_id
-  df$author<-x$author
-  df$url<-x$url
-  df$date<-x$date
+#  df$timestamp<-x$timestamp
+#  df$com_id<-x$comment_id
+  #df$author<-x$author
+  #df$url<-x$url
+  df$url_id<-chunk
+  #df$date<-x$date
   df$votes<-x$upvotes
   return(df)
 }
-l.df<-length(com.df$url)
-chunks<-ceiling(l.df/1000)
+#l.df<-length(com.df$url)
+#chunks<-ceiling(l.df/1000)
 # Create the vector
-vector <- 1:l.df
+#vector <- 1:l.df
 
 # Define the chunk size
-chunk_size <- 1000  # Example chunk size
+#chunk_size <- 1000  # Example chunk size
 
 # Function to split the vector into chunks of equal length
 split_into_chunks <- function(vec, chunk_size) {
@@ -53,7 +59,7 @@ split_into_chunks <- function(vec, chunk_size) {
 }
 
 # Split the vector into chunks
-chunks <- split_into_chunks(vector, chunk_size)
+#chunks <- split_into_chunks(vector, chunk_size)
 
 cleancomments<-c("\034","\035","&gt;","\036","&amp","\031","\023","\030","\005","\004","\024","\002","/u","&lt","&lt;","&gt","<")
 cleancomments<-paste0(cleancomments,collapse = "|")
@@ -67,51 +73,88 @@ k<-2
 # com.vrt.es.1<-com.vrt.es.1[,df.ns]
 # colnames(com.vrt.es.1)<-names(df.ns)
 k<-1
-chunk<-1
+#chunk<-1
 head(chunks[1])
 #for(k in 2:1000){
 ### > TODO: loop redo, saves false
+url.u<-unique(com.df$url)
+com.df$url_id<-""
+k<-1
+df.ns<-c(token=6,pos=8,lemma=7,feats=10,sentence=4,tok_id=5,head_tok_id=11,dep_rel=12,url_id=19,par_id=2,sent_id=3,timestamp=15,date=20,com_id=16,author=17,votes=21,url=18)
+df.ns<-c(token=6,pos=8,lemma=7,feats=10,sentence=4,tok_id=5,head_tok_id=11,dep_rel=12,par_id=2,sent_id=3,votes=16)
+for(k in 1:length(url.u)){
+  url<-url.u[k]
+  com.df$url_id[com.df$url==url]<-k
+  
+}
 for(chunk in 1:length(chunks)){
-  for(k in 2:length(chunk.range)){
-  
+ # for(k in 2:length(chunk.range)){
+#  k<-1
   chunk.range<-chunks[[chunk]]
-  for(k in 1:length(chunk.range)){
-    chunk.df<-com.df[chunk.range[k],]
-    df.ns<-c(token=6,pos=8,lemma=7,feats=10,sentence=4,tok_id=5,head_tok_id=11,dep_rel=12,doc_id=1,par_id=2,sent_id=3,timestamp=15,date=19,com_id=16,author=17,votes=20,url=18)
-  mode(df.ns)
+  url<-1
+  chunk
+   for(url in 1:length(url.u)){
+    url.df<-com.df[com.df$url_id==url,]
+    url.df
+ # mode(df.ns)
   
-  com.vrt.es.1<-get.ann.df_ann(chunk.df[1,],cleancomments)
-  com.vrt.es.1<-rbind("",com.vrt.es.1,"")
-  com.vrt.es.1$token[1]<-'<com id="1">'
-  com.vrt.es.1$token[length(com.vrt.es.1$token)]<-"</com>"
-  com.vrt.es.1<-com.vrt.es.1[,df.ns]
-  colnames(com.vrt.es.1)<-names(df.ns)
-  #k<-2
-  com.ann<-get.ann.df_ann(chunk.df[k,],cleancomments)
+  # com.vrt.es.1<-get.ann.df_ann(chunk.df[1,],cleancomments,chunk=url)
+  # doc.id<-paste0('<com id="url-',com.vrt.es.1$url_id[1],'.',com.vrt.es.1$com_id[1],'">')
+  # com.vrt.es.1<-rbind("",com.vrt.es.1,"")
+  # 
+  # com.vrt.es.1$token[1]<-doc.id
+  # com.vrt.es.1$token[length(com.vrt.es.1$token)]<-"</com>"
+  # com.vrt.es.1<-com.vrt.es.1[,df.ns]
+  # colnames(com.vrt.es.1)<-names(df.ns)
+  # k<-1
+  k
+  chunk.df<-url.df
+  length(chunk.df)
+  com<-1
+  #com
+  for(com in 1:length(chunk.df$url)){
+    chunk.df
+    chunk <- paste0(url,'.',com)
+  chunk
+    com.ann<-get.ann.df_ann(chunk.df[com,],cleancomments,chunk)
+  com.ann
+#  doc.id<-paste0('<com id="',com.ann$url_id[com],'">')
+  doc.id<-paste0('<com id="',chunk,'">')
   com.ex<-rbind("",com.ann,"")
-  m<-colnames(com.ex)=="token"
+  length(df.ns)
+  length(com.ann)
+  
+  #m<-colnames(com.ex)=="token"
   com.ann<-com.ex[,df.ns]
   colnames(com.ann)<-names(df.ns)
-  head.doc<-com.ann[1,]
+  com.ann$token[1]<-doc.id
+  com.ann$token[length(com.ann$token)]<-'</com>'
+  #head.doc<-com.ann[1,]
   #head.doc[1,]<-NA
-  head.doc[1,]
-  foot.doc<-head.doc
-  head.doc$token<-paste0('<com id="',k,'">')
-  foot.doc$token<-'</com>'
-  doc.ann<-rbind(head.doc,com.ann,foot.doc)
-  com.vrt.es.1<-rbind(com.vrt.es.1,doc.ann)
-  for(na in 1:length(com.vrt.es.1)){
+  #head.doc[1,]
+  #foot.doc<-head.doc
+  #head.doc$token<-doc.id
+  #foot.doc$token<-'</com>'
+  #doc.ann<-rbind(head.doc,com.ann,foot.doc)
+  #doc.ann
+  #com.vrt.es.1<-rbind(com.vrt.es.1,doc.ann)
+  com.vrt.es.1<-com.ann
+    for(na in 1:length(com.vrt.es.1)){
   m<-is.na(com.vrt.es.1[,na])
   com.vrt.es.1[m,na]<-""
-  }
-  cat("processing:",chunk,".",k,"\n")  
+    }
+  #com.vrt.es.1
+  cat("processing, chunk:",chunk,"\n")  
 #}
 # chk unique tokens:
-com.vrt.es.2<-com.vrt.es.1[!is.na(com.vrt.es.1$token),]
+#com.vrt.es.2<-com.vrt.es.1[!is.na(com.vrt.es.1$token),]
+com.vrt.es.2<-com.vrt.es.1
 com.vrt.es.2<-com.vrt.es.2[com.vrt.es.2$token!="",]
-m<-grepl("<com id",com.vrt.es.2$token)
+m<-grepl("<com id|</com>",com.vrt.es.2$token)
+sum(m)
 com.vrt.es.2[m,2:length(com.vrt.es.2)]<-""
-m2<-com.vrt.es.2$token=="<s>"|com.vrt.es.2$token=="</s>"
+m2<-grepl("<s time|</s>",com.vrt.es.2$token)
+sum(m2)
 com.vrt.es.2[m2,2:length(com.vrt.es.2)]<-""
 sum(m2)
 com.vrt.es.3<-com.vrt.es.2
@@ -124,14 +167,17 @@ length(unique(com.vrt.es.2$lemma)) # 1:1000:7707.7540.7543
 # ttb
 #?write.table
  vrt.dir<-paste(wd,"vrt3",sep = "/")
- vrt.ns<-paste0(vrt.dir,"/com.vrt3.c",chunk,".vrt")
- #dir.create(vrt.dir)
+ 
+ vrt.ns<-paste0(vrt.dir,"/com.vrt3.url",url,".vrt")
+ vrt.ns
+ dir.create(vrt.dir)
 write.table(com.vrt.es.3,vrt.ns,sep = "\t",row.names = F,append=T,col.names = F,quote = F)
-  }
+  } # comment loop
+   } # url loop
 xml<-readLines(vrt.ns)
-xml<-c('<?xml version="1.0" encoding="UTF-8"?>',xml)
+xml<-c('<?xml version="1.0" encoding="UTF-8"?>',paste0('<doc id="chunk-',chunk,'">'),xml,'</doc>')
 writeLines(xml,vrt.ns)
-
+chunk
 }
 ########################################################
 ########################################################
---
+++ intLX/cwb-wrap.R
@@ -0,0 +1,186 @@
+# registry directory and cpos_total will be needed in examples
+cpos_total <- cl_attribute_size(
+  corpus = "REUTERS", attribute = "word",
+  attribute_type = "p", registry = get_tmp_registry()
+)
+
+# decode the token stream of the corpus (the quick way)
+token_stream_str <- cl_cpos2str(
+  corpus = "REUTERS", p_attribute = "word",
+  cpos = seq.int(from = 0, to = cpos_total - 1),
+  registry = get_tmp_registry()
+)
+
+# decode the token stream (cpos2id first, then id2str)
+token_stream_ids <- cl_cpos2id(
+  corpus = "REUTERS", p_attribute = "word",
+  cpos = seq.int(from = 0, to = cpos_total - 1),
+  registry = get_tmp_registry()
+)
+token_stream_str <- cl_id2str(
+  corpus = "REUTERS", p_attribute = "word",
+  id = token_stream_ids, registry = get_tmp_registry()
+)
+
+# get corpus positions of a token
+token_to_get <- "oil"
+id_oil <- cl_str2id(
+  corpus = "REUTERS", p_attribute = "word",
+  str = token_to_get, registry = get_tmp_registry()
+)
+cpos_oil <- cl_id2cpos <- cl_id2cpos(
+  corpus = "REUTERS", p_attribute = "word",
+  id = id_oil, registry = get_tmp_registry()
+)
+
+# get frequency of token
+oil_freq <- cl_id2freq(
+  corpus = "REUTERS", p_attribute = "word", id = id_oil, registry = get_tmp_registry()
+)
+length(cpos_oil) # needs to be the same as oil_freq
+
+# use regular expressions 
+ids <- cl_regex2id(
+  corpus = "RGRDF2", p_attribute = "word",
+  regex = "M.*", registry = get_tmp_registry()
+)
+m_words <- cl_id2str(
+  corpus = "REUTERS", p_attribute = "word",
+  id = ids, registry = get_tmp_registry()
+)
+m_words <- cl_id2str(
+  corpus = "RGRDF2", p_attribute = "c",
+  id = ids, registry = get_tmp_registry()
+)
+check_id(corpus = corpus,"lemma",10)
+check_registry(rg)
+check_corpus("RGRDF2",rg,T,T)
+check_registry(rg)
+cpos_total <- cl_attribute_size(
+  corpus = "RGRDF2", attribute = "lemma",
+  attribute_type = "p", registry = get_tmp_registry()
+)
+m_words
+
+
+### from start
+library(RcppCWB)
+#cfile<-"~/boxHKW/21S/DH/local/SPUND/intLX/com.vrt.1-1000.txt"
+data_dir = "~/pro/cwb/indexed/rdf3f"
+vrt_dir<-"~/boxHKW/21S/DH/local/SPUND/intLX/vrt2"
+list.files(vrt_dir)
+dir.create(data_dir)
+cwb_encode(
+  "RDF2",
+  registry = Sys.getenv("CORPUS_REGISTRY"),
+  data_dir = data_dir,
+  vrt_dir="~/boxHKW/21S/DH/local/SPUND/intLX/vrt2",
+  encoding = "utf8",
+  p_attributes = c("word", "lemma", "tag"),
+  s_attributes = list(text ="id",s=character()),
+  skip_blank_lines = TRUE,
+  strip_whitespace = TRUE,
+  xml = T,
+  quietly = FALSE,
+  verbose = FALSE
+)
+# perform cwb_makeall (equivalent to cwb-makeall command line utility)
+cwb_makeall(corpus = "BTMIN", p_attribute = c("word"), registry = sysregdir)
+cwb_makeall(corpus = "RDF2", p_attribute = c("word"), registry = sysregdir)
+cl_load_corpus("RDF2", registry = sysregdir)
+cqp_load_corpus("RDF2", registry = sysregdir)
+# see whether it works
+ids_sentence_1 <- cl_cpos2id(
+  corpus = "RDF2", p_attribute = "word", registry = get_tmp_registry(),
+  cpos = 0:83
+)
+tokens_sentence_1 <- cl_id2str(
+  corpus = "RDF2", p_attribute = "word",
+  registry = get_tmp_registry(), id = ids_sentence_1
+)
+sentence <- gsub("\\s+([\\.,])", "\\1", paste(tokens_sentence_1, collapse = " "))
+sentence
+##################
+cqp_list_corpora()
+cqp_load_corpus("RDF2",sysregdir)
+cqp_load_corpus("REUTERS",sysregdir)
+cqp_load_corpus("BTMIN",sysregdir)
+cqp_load_corpus("REDDIT",pregdir)
+query = '[word=".*"&pos="N.*"];'
+query<-'[word="fr.*"];'
+query = '[word=".*"&lemma="fr.*"][word=".*"&pos=".*"];'
+query = '[word=".*"&lemma="fr.*"][pos="PRON.*"];'
+cqp_query("REDDIT",pregdir,'show -pos;')
+cqp_query("REDDIT",pregdir,query)
+# Get the number of matches
+num_matches <- cqp_subcorpus_size("REDDIT","QUERY")
+
+# Extract the matches
+matches <- cqp_dump_subcorpus(corpus, 0, num_matches - 1)
+
+# Convert the matches to a data frame
+results_df <- as.data.frame(matches, stringsAsFactors = FALSE)
+
+# Print the data frame
+print(results_df)
+cqp_query("REDDIT",pregdir,"LAST;")
+cqp_query("BTMIN",pregdir,query)
+query = '[word=".*"&pos=".*"];'
+
+cqp_query("BTMIN",sysregdir,'show +lemma;')
+cqp_query("BTMIN",sysregdir,query)
+sysregdir<-Sys.getenv("CORPUS_REGISTRY")
+writeLines(readLines(Sys.getenv("CORPUS_REGISTRY")),"~/pro/cwb/registry/rdftemp")
+writeLines(readLines(paste(sysregdir,list.files(sysregdir)[1],sep="/")),"~/pro/cwb/registry/btmin")
+### sample encode
+library(RcppCWB)
+demodata = "~/pro/cwb/indexed/btmintb"
+vrt_dir<-"~/boxHKW/21S/DH/local/SPUND/intLX/vrt2"
+list.files(vrt_dir)
+dir.create(demodata)
+pregdir<-"/Users/guhl/pro/cwb/registry"
+cwb_encode(
+  corpus = "BTMINT",
+  registry = pregdir,
+  vrt_dir = system.file(package = "RcppCWB", "extdata", "vrt"),
+  data_dir = demodata,
+  p_attributes = c("word", "pos", "lemma"),
+  s_attributes = list(
+    plenary_protocol = c(
+      "lp", "protocol_no", "date", "year", "birthday", "version",
+      "url", "filetype"
+    ),
+    speaker = c(
+      "id", "type", "lp", "protocol_no", "date", "year", "ai_no", "ai_id",
+      "ai_type", "who", "name", "parliamentary_group", "party", "role"
+    ),
+    p = character()
+  )
+)
+pregdir<-"/Users/guhl/pro/cwb/registry"
+demodata = "~/pro/cwb/indexed/rdf3g"
+vrt_dir<-"~/boxHKW/21S/DH/local/SPUND/intLX/vrt3"
+list.files(vrt_dir)
+list.files(demodata)
+dir.create(demodata)
+cwb_encode(
+  corpus = "REDDIT",
+  registry = pregdir,
+  vrt_dir = vrt_dir,
+  data_dir = demodata,
+  p_attributes = c("word", "pos", "lemma"),
+  s_attributes = list(
+    doc = c("id"),
+    com = c("id"),
+    s = character()
+  ),
+  xml = T,
+  quietly = F
+)
+cwb_makeall(corpus = "REDDIT", p_attribute = c("word"), registry = pregdir)
+
+vrt_dir = system.file(package = "RcppCWB", "extdata", "vrt")
+vrt_dir
+list.files(vrt_dir)
+writeLines(readLines(paste(vrt_dir,list.files(vrt_dir)[1],sep="/")),"~/pro/cwb/rcppdemo.xml")
+
