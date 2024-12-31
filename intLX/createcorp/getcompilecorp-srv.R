220b3316b692803111c126893bd0bb7d6341325e
estee <91368979+esteeschwarz@users.noreply.github.com>
2024-12-31 13:16:31 +0100

    upd, for pull scripts into devbox

--- intLX/createcorp/getcompilecorp-srv.R
+++ intLX/createcorp/getcompilecorp-srv.R
@@ -3,6 +3,7 @@
 #source("~/Documents/GitHub/SPUND-LX/intLX/createcorp/functions.R")
 ### embed script function
 library(udpipe)
+model.dir<-"./modeldir"
 get.ann.df<-function(model.dir,input,output){
   t<-input
   #t<-readLines(text)
@@ -15,7 +16,7 @@ get.ann.df<-function(model.dir,input,output){
   model<-udpipe::udpipe_load_model(model)
   pos1<-udpipe_annotate(model,t)
   pos.df<-as.data.frame(pos1)
-#  pos.df$timestamp<-unique(meta$timestamp)
+  #  pos.df$timestamp<-unique(meta$timestamp)
   return(pos.df)
 }
 cat("function(model.dir,input(character),output(F))\n")
@@ -38,7 +39,7 @@ get.sent.div<-function(x,id,n){
   m<-df$sentence_id==id
   cat("\nsent:",id,".",n,"\n")
   df.es<-df[m,]
-  df.es$doc_id<-paste0("pid:",df.es$doc_id,id)
+  df.es$pid<-paste0("pid:",df.es$uid,".",id,".",n)
   
   s.id<-id
   df.start<-df.es[1,]
@@ -60,28 +61,28 @@ get.sent.div<-function(x,id,n){
 get.doc.div<-function(x,id,n){
   df<-x
   m<-df$doc_id==id
-  cat("\ndoc:",id,".",n,"\n")
+  cat("\nrundoc:",run,id,".",n,"of",length(url.sub.df$url),"doc elements\n")
   #m<-doc.u==p
- # doc.here<-which(m)
+  # doc.here<-which(m)
   #m
   df.es<-df[m,c(6,2:5,7:length(df))]
- # m2<-colnames(df.ex)=="misc"|colnames(df.ex)=="deps"
- # which(m2)
+  # m2<-colnames(df.ex)=="misc"|colnames(df.ex)=="deps"
+  # which(m2)
   #sum(m)
-#    m2<-colnames(df.es)=="misc"|colnames(df.es)=="deps"
-#    m3<-c(1:length(m2))[!m2]
-#    m3
-#    df.es<-df.es[,m3]
-# #   df.es$doc_id<-paste0(id,".",df.es$doc_id)
-
+  #    m2<-colnames(df.es)=="misc"|colnames(df.es)=="deps"
+  #    m3<-c(1:length(m2))[!m2]
+  #    m3
+  #    df.es<-df.es[,m3]
+  # #   df.es$doc_id<-paste0(id,".",df.es$doc_id)
+  
   sent.id<-df.es$sentence_id
   sent.id.u<-unique(sent.id)
   sent.df<-pblapply(seq_along(sent.id.u), function(i) {
     get.sent.div(df.es,sent.id.u[[i]], i)
   })
   df.es<-data.frame(abind(sent.df,along = 1))
-####### > inserts <text> element, obsolete
-    # df.start<-df.es[1,]
+  ####### > inserts <text> element, obsolete
+  # df.start<-df.es[1,]
   # df.start[1,]<-""
   # # s.tag<-paste0('<s timestamp="',x$timestamp,'" sent_id ="',paste0(chunk,'.',x$comment_id),'.',s.id,'" author="',x$author,'" url="',x$url,'" url_id="',chunk,'" date="',x$date,'" upvotes="',x$upvotes,'">')
   #rownames raw: "doc_id" "paragraph_id" "sentence_id" "sentence" "token_id" "token" "lemma" "upos" "xpos" "feats" "head_token_id" "dep_rel" "deps" "misc"
@@ -103,13 +104,14 @@ fetch.pos<-function(file,i,data){
   # t<-readLines(file)
   t<-file
   fn<-i
-  pos.df<-get.ann.df(model.dir = "~/Documents/GitHub/SPUND-LX/intLX/createcorp/modeldir",input = t,output = F)
+  pos.df<-get.ann.df(model.dir = model.dir,input = t,output = F)
   author<-data[[1]]
   cat("\ndata:",fn,"author:",author,"timestamp:",data[[2]],"\n")
   #print(author)
   print(data)
   print(timestamp<-data[[2]])
   #pos.df$doc_id<-paste0(i,".",gsub("doc","",pos.df$doc_id))
+  pos.df$uid<-paste0("run",run,"-url",i)
   pos.df$doc_id<-i
   #pos.df$author<-author
   pos.df$timestamp<-timestamp
@@ -126,12 +128,12 @@ fetch.pos<-function(file,i,data){
   df.write<-as.data.frame(abind(df.ex.l,along = 1))
   # mna<-is.na(df.write$sentence)
   # df.ex.l<-df.write[!mna,]
-  df.write<-rbind(c(paste0('<doc id="',fn,'" url="',url,'" author="',author,'">'),
+  df.write<-rbind(c(paste0('<doc id="',run,'-',fn,'" url="',url,'" author="',author,'">'),
                     rep("",length(df.write)-1)),df.write,
                   c('</doc>',rep("",length(df.write)-1)))
-### > this to write vrt for each url
-    # write.table(df.write,paste0("~/Documents/GitHub/SPUND-LX/intLX/createcorp/testout/vrt.f-",fn,".csv"),sep = "\t",quote = F,row.names = F,col.names = F,na="")
-###
+  ### > this to write vrt for each url
+  # write.table(df.write,paste0("~/Documents/GitHub/SPUND-LX/intLX/createcorp/testout/vrt.f-",fn,".csv"),sep = "\t",quote = F,row.names = F,col.names = F,na="")
+  ###
   return(abind(df.write,along = 1))
 }
 #?write.table
@@ -147,52 +149,71 @@ fetch.pos<-function(file,i,data){
 #   my_function(my_list[[i]], i)
 # })
 ### now with reddit dataframe
-wd<-"~/boxHKW/21S/DH/local/SPUND/intLX"
+wd<-getwd()
 load(paste(wd,"reddit_15494.df.RData",sep = "/"))
 urls<-com.df$url
 url.u<-unique(urls)
-url.sub<-url.u[1:200] # 1:10:411 obs
-url.sub.df<-com.df[com.df$url%in%url.sub,]
-
-# df.ex.l<-pblapply(seq_along(fns), function(i) {
-#   fetch.pos(fns[[i]], i)
-# })
-##################################################################
-### > here adapt change to length of url.sub.df to get all url pos
-# df.ex.url<-pblapply(seq_along(1:2),function(i){
-#   cat("url:",url,"\n")
-df.ex.l<-pblapply(seq_along(url.sub.df$url), function(i) {
-#  df.ex.l<-pblapply(seq_along(1:length(url.sub)), function(i) {
-    #comments<-url.sub.df$comment[url.sub.df$url==url.sub[i]]
-  comments<-url.sub.df[i,]
-  #print(length(comments))
-#  author<-url.sub.df$author[url.sub.df$url==url.sub[i]]
- # timestamp<-url.sub.df$timestamp[url.sub.df$url==url.sub[i]]
-  print(author<-comments$author)
-  print(timestamp<-comments$timestamp)
-  print(score<-comments$score)
-  print(com_id<-comments$comment_id)
-  print(url<-comments$url)
-  print(date<-comments$date)
+out.dir<-paste(getwd(),"corpora/reddit/vertical/vrt6",sep="/")
+out.ns<-paste(out.dir,"source",sep="/")
+#file.create(out.ns)
+# url.sub<-url.u[1:200] # 1:10:411 obs
+# url.sub.df<-com.df[com.df$url%in%url.sub,]
+run<-23 # killed on server. at 63 (last comment)
+for (run in 11:50){
   
-  print(comment<-comments$comment)
-   # print(i)
+  #for (run in 1:length(url.u)){
+  url.sub<-url.u[run] # 1:10:411 obs
+  url.sub.df<-com.df[com.df$url%in%url.sub,]
+  # df.ex.l<-pblapply(seq_along(fns), function(i) {
+  #   fetch.pos(fns[[i]], i)
+  # })
+  ##################################################################
+  ### > here adapt change to length of url.sub.df to get all url pos
+  # df.ex.url<-pblapply(seq_along(1:2),function(i){
+  #   cat("url:",url,"\n")
+  df.ex.l<-pblapply(seq_along(url.sub.df$url), function(i) {
+    #  df.ex.l<-pblapply(seq_along(1:length(url.sub)), function(i) {
+    #comments<-url.sub.df$comment[url.sub.df$url==url.sub[i]]
+    comments<-url.sub.df[i,]
+    #print(length(comments))
+    #  author<-url.sub.df$author[url.sub.df$url==url.sub[i]]
+    # timestamp<-url.sub.df$timestamp[url.sub.df$url==url.sub[i]]
+    print(author<-comments$author)
+    print(timestamp<-comments$timestamp)
+    print(score<-comments$score)
+    print(com_id<-comments$comment_id)
+    print(url<-comments$url)
+    print(date<-comments$date)
+    
+    print(comment<-comments$comment)
+    # print(i)
     #print(comment)
     com.x<-fetch.pos(comment, i,data=list(author=author,timestamp=timestamp,date=date,url=url,score=score,com_id=com_id))
-  return(com.x)
-})
-#})
-#df.ex<-abind::abind(df.ex.l[[1]],along = 1)
-df.ex<-abind::abind(df.ex.l,along = 1)
-
-df.ex<-data.frame(df.ex)
-m2<-colnames(df.ex)=="misc"|colnames(df.ex)=="deps"
-m3<-c(1:length(m2))[!m2]
-m3
-df.ex<-df.ex[,m3]
-write.table(df.ex,"~/Documents/GitHub/SPUND-LX/intLX/createcorp/vertical/source/004-comdf-url-200.csv",sep = "\t",quote = F,row.names = F,col.names = F,na="")
-### wks.
-#}) #end url lapply
+    return(com.x)
+  })
+  #})
+  #df.ex<-abind::abind(df.ex.l[[1]],along = 1)
+  df.ex<-abind::abind(df.ex.l,along = 1)
+  
+  df.ex<-data.frame(df.ex)
+  m2<-colnames(df.ex)=="misc"|colnames(df.ex)=="deps"|colnames(df.ex)=="sentence"
+  m3<-c(1:length(m2))[!m2]
+  m3
+  df.ex<-df.ex[,m3]
+  # out.dir<-paste(getwd(),"corpora/reddit/vertical/vrt6",sep="/")
+  # out.ns<-paste(out.dir,"source",sep="/")
+  print(size<-file.size(out.ns)/1000/100)
+  write.table(df.ex,out.ns,sep = "\t",quote = F,
+              row.names = F,col.names = F,na="",append=T)
+  ### wks.
+  #}) #end url lapply
+  save(df.ex.l,file=paste(wd,"red_df.ex.el.RData",sep="/"))
+} # end url/run loop
+################################################
+# command: make execute CMD="compilecorp --no-ske testvrt"
+call.noske<-'make execute CMD="compilecorp --no-ske --recompile-corpus reddit4"'
+system(call.noske)
+print("process finished.")
 
 
 
