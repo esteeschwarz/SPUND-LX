cf082e76009431dc8249db4a2286a7cd32686cfd
estee <91368979+esteeschwarz@users.noreply.github.com>
2025-01-19 16:17:49 +0100

    wolf essai

--- szondi/WITprose/wolf-analysis.R
+++ szondi/WITprose/wolf-analysis.R
@@ -6,7 +6,8 @@
 fr.ref<-load("/Users/guhl/boxHKW/21S/DH/local/SPUND/corpuslx/DeReKo.freq.ref.RData")
 k2<-read.csv("~/Documents/static/server/ada/es/r/knitessai/db/wolfdb003.csv")
 k2<-k2$content[k2$book=="FF"]  
-
+#k2
+k2
 table(names(k1$token))
 length(k1$token)
 length(unique(k1$token))
@@ -31,14 +32,15 @@ get.keys<-function(){
   #reference_corpus <- "This is a reference corpus. It contains many words. This corpus is used for comparison."
   
   # Create a Corpus object for both texts
-  given_corpus <- Corpus(VectorSource(given_text))
+ # given_corpus <- Corpus(VectorSource(given_text))
   #reference_corpus <- Corpus(VectorSource(reference_corpus))
   
-  g2<-Corpus(VectorSource(k2$token))
+  #g2<-Corpus(VectorSource(k2$token))
+  g2<-Corpus(VectorSource(k2))
   given_corpus<-g2
   # Tokenize and clean the text
   clean_corpus <- function(corpus) {
-    corpus <- tm_map(corpus, content_transformer(tolower))
+   # corpus <- tm_map(corpus, content_transformer(tolower)) # no difference: wolf text wo capital letters
     corpus <- tm_map(corpus, removePunctuation)
     corpus <- tm_map(corpus, removeNumbers)
     corpus <- tm_map(corpus, removeWords, stopwords("en"))
@@ -56,29 +58,58 @@ get.keys<-function(){
   #reference_dtm <- DocumentTermMatrix(reference_corpus)
   
   # Convert to data frames
-  #given_freq <- as.data.frame(as.matrix(given_dtm))
+  given_freq <- as.data.frame(as.matrix(given_dtm))
   # no
-  library(collostructions)
-  given_freq<-freq.list(k2$token)
+  #library(collostructions)
+  #given_corpus
+  #given_freq<-freq.list(k2$token)
   #reference_freq <- as.data.frame(as.matrix(reference_dtm))
   
   # Sum the frequencies of each token
-  #given_freq <- colSums(given_freq)
+  given_freq <- colSums(given_freq)
   #reference_freq <- colSums(reference_freq)
   #reference_freq<-k1
   reference_freq<-k1[,c(1,4)]
+  ### tolower tokens:
+  lowcaps<-lapply(reference_freq$token, tolower)
+  reference_freq$token_lc<-lowcaps
+  reference_freq.dp<-duplicated(lowcaps)
+  sum(reference_freq.dp)
+  # Sample dataframe
+  df <- data.frame(
+    token = c("a", "b", "a", "c", "b", "d"),
+    frequency = c(1, 2, 3, 4, 5, 6)
+  )
+  
+  # Merge duplicated tokens by summing their frequencies
+  merged_df <- df %>%
+    group_by(token) %>%
+    summarise(frequency = sum(frequency))
+  
+  #print(merged_df)
+  ref_freq_merged<- reference_freq %>%
+    group_by(token_lc) %>%
+    summarise(frequency =sum(freq))
+  
   head(given_freq)
-  head(reference_freq)
-  colnames(given_freq)<-c("token","freq")
-  colnames(reference_freq)<-c("token","freq")
-  jfreq<-join.freqs(given_freq,reference_freq,all = T)
-  c1<-collex.dist(jfreq)
-  c1%>%arrange(desc(COLL.STR.LOGL))
+  head(ref_freq_merged)
+  colnames(ref_freq_merged)<-c("token","freq")
+  #colnames(reference_freq)<-c("token","freq")
+  given_freq <- data.frame(token = names(given_freq), freq = given_freq)
+  m<-given_freq$token%in%ref_freq_merged$token
+  sum(m)
+  tok.out<-given_freq$token[!m]
+  tok.out.df<-data.frame(token=tok.out,freq=1)
+  colnames(tok.out.df)==colnames(ref_freq_merged)
+  ref_freq_plus<-rbind(ref_freq_merged,tok.out.df)
+    #jfreq<-join.freqs(given_freq,reference_freq,all = T)
+  #c1<-collex.dist(jfreq)
+  #c1%>%arrange(desc(COLL.STR.LOGL))
   # Convert to data frames
-  #given_freq <- data.frame(token = names(given_freq), freq = given_freq)
   #reference_freq <- data.frame(token = names(reference_freq), freq = reference_freq)
   # Merge the frequency data frames
-  freq_comparison <- merge(given_freq, reference_freq, by = "token", all = F)
+#  freq_comparison <- merge(given_freq, reference_freq, by = "token", all = F)
+  freq_comparison <- merge(given_freq, ref_freq_plus, by = "token", all = F)
   colnames(freq_comparison) <- c("token", "given_freq", "reference_freq")
   freq_comparison$token[52]==freq_comparison$token[75]
   # Replace NA values with 0
@@ -98,4 +129,17 @@ get.keys<-function(){
   print(freq_comparison)
 }
 get.keys()
+# wks.
 
+######
+# how:
+# 1. get annotated df, define pos pattern, compare to poetry/prose patterns used in reference corpus
+# 2. 
+ref.pos.folder<-"~/Documents/GitHub/clones/DLK/DLK/meterized/json_DLK_v6"
+f<-list.files(ref.pos.folder)
+f.ns<-paste(ref.pos.folder,f,sep="/")
+library(jsonlite)
+x<-fromJSON(readLines(f.ns[1]),simplifyDataFrame = T,flatten = T)
+#jsonlite::
+x1<-abind(unlist(x$dta.poem.1$poem),along = 0)
+x1<-lapply(x$dta.poem.1$poem, data_frame)
