a71bff80d81e303cfe7eab0f1a2af1cc636c81d9
estee <91368979+esteeschwarz@users.noreply.github.com>
2025-03-21 17:28:15 +0100

    upd idioms

--- intLX/HA/ann-idioms.py
+++ intLX/HA/ann-idioms.py
@@ -2,12 +2,14 @@ import sys
 
 # Print the path of the currently running Python interpreter
 print("Path of the Python interpreter:", sys.executable)
+
+#################################
 from datasets import load_dataset
 
 # Load the dataset
-dataset = load_dataset("davidstap/IdiomsInCtx-MT")
+dataset = load_dataset("davidstap/IdiomsInCtx-MT","de-en")
 
 # Inspect the dataset
-print(dataset["train"][0])  # View the first example
+print(dataset["test"]["de"][0])  # View the first example
 
 
