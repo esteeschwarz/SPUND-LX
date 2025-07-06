d2b20f79b31a7e5b580521622d9cba8ccb792a03
estee <91368979+esteeschwarz@users.noreply.github.com>
2025-07-06 16:17:10 +0200

    kollo

--- psych/HA/eval-002.R
+++ psych/HA/eval-002.R
@@ -2,41 +2,48 @@
 #15273.reddit.stats.analysis
 ############################
 dfa<-read.csv(paste0(Sys.getenv("GIT_TOP"),"/SPUND-LX/psych/HA/eval-002.csv"))
-dfa<-qltdf
+dfa<-read.csv(paste0(Sys.getenv("GIT_TOP"),"/SPUND-LX/psych/HA/eval-003.csv"))
+#dfa<-qltdf
 queries<-unique(dfa$q)
 #df<-q.all.df
 qn<-unique(dfa$q)
 c<-letters[1:length(qn)]
-for(k in 1:length(qn)){
-  m<-dfa$q==qn[k]
-  dfa$q[m]<-c[k]
-}
+# for(k in 1:length(qn)){
+#   m<-dfa$q==qn[k]
+#   dfa$q[m]<-c[k]
+# }
 unique(dfa$q)
 
 Y <- dfa$dist           # Dependent variable
-group <- dfa$target       # Assuming the second column is the grouping variable
+mean(Y[dfa$target=="obs"])
+mean(Y[dfa$target=="ref"])
+target <- dfa$target       # Assuming the second column is the grouping variable
 
-group <- as.factor(group)
-table(group)
+target <- as.factor(target)
+table(target)
 
-anova_model <- aov(Y ~ group, data = dfa)
-anova_model <- aov(Y ~ group*q, data = dfa)
+#anova_model <- aov(Y ~ target, data = dfa)
+anova_model <- aov(Y ~ target*q, data = dfa)
 #anova_model <- aov(Y ~ group*q, data = dfa)
-summary(anova_model)
+anova.sum<-summary(anova_model)
+anova.sum
 library(lmerTest)
 #scipen(999)
-lm1<-lmer(dist~target*q+(1|mf_rel)+(1|range)+(1|ld),dfa)
-lm2<-lmer(dist~target+range+(1|lemma),dfa)
+#lm1<-lmer(dist~target*q+(1|mf_rel)+(1|range)+(1|ld),dfa)
+#lm2<-lmer(dist~target+range+(1|lemma),dfa)
 lm2<-lmer(dist~target*q+range+(1|lemma),dfa)
+lm2<-lmer(dist~target*q+range+(1|lemma),dfa)
+#lm2<-lm(dist~target*q+range,dfa) # without random effects
 summary(lm2)
 lm2.summ<-summary(lm2)
 lm2.summ
-an2.summ<-anova(lm2)
-an2.summ
+anlm.summ<-anova(lm2)
+anlm.summ
 lm.summ<-summary(lm1)
-an.summ<-anova(lm1)
-lm.summ
-an.summ
+#an.summ<-anova(lm1)
+#lm.summ
+anlm.summ
+
 #wks.
 # #get mean:
 # m.target<-median(dfa$dist[dfa$target=="obs"])
@@ -71,26 +78,90 @@ get.m.df<-function(dfa){
 # Read the data
 #df <- read.csv("eval-001.csv")
 df.eval<-get.m.df(dfa)
-sum(df.eval$mean[df.eval$target=="obs"])
-sum(df.eval$mean[df.eval$target=="ref"])
+df.eval$mean[df.eval$target=="obs"]
+df.eval$mean[df.eval$target=="ref"]
 dfe<-df.eval
 
 # Ensure q is ordered a-f
 dfe$q <- factor(dfe$q, levels = c("a", "b", "c", "d", "e", "f"))
 
+
+rest.fun<-function(){
+#gpt manual p
+  data<-dfa
+  
+  # Center covariates
+  # t3<-
+  data$range_c    <- data$range    - mean(data$range[data$q=="a"]) # level intercept for conditions b-f 
+  # Corpus dummy
+  data$corpusB <- ifelse(data$target == 'ref', 1, 0)
+  data$corpusA <- ifelse(data$target == 'obs', 1, 0)
+  
+  # Dummy code condition (a-e) into 4 dummy vars (base = 'a')
+  #data$cond_a <- ifelse(data$q == 'a', 1, 0)
+  data$cond_b <- ifelse(data$q == 'b', 1, 0)
+  data$cond_c <- ifelse(data$q == 'c', 1, 0)
+  data$cond_d <- ifelse(data$q == 'd', 1, 0)
+  data$cond_e <- ifelse(data$q == 'e', 1, 0)
+  data$cond_f <- ifelse(data$q == 'f', 1, 0)
+  
+
+  X <- as.matrix(cbind(
+    1,
+    data$corpusB,
+    # data$range_c,
+    #data$corpsize_c,
+    #data$m_rel_c,
+  #    data$cond_a,
+    data$cond_b,
+    data$cond_c,
+    data$cond_d,
+    data$cond_e,
+    data$cond_f
+  ))
+  qr(X)$rank  # should equal ncol(X) = 8
+  
+  Y <- data$dist
+  
+  XtX <- t(X) %*% X
+  XtY <- t(X) %*% Y
+  beta_hat <- solve(XtX) %*% XtY
+  residuals <- Y - X %*% beta_hat
+  n <- nrow(X)
+  k <- ncol(X)
+  sigma2_hat <- sum(residuals^2) / (n - k)
+  
+  cov_beta <- sigma2_hat * solve(XtX)
+  std_errors <- sqrt(diag(cov_beta))
+  
+  t_value <- beta_hat[2] / std_errors[2]
+  df <- n - k
+  p_value <- 2 * pt(-abs(t_value), df)
+  # 1st: 0.352
+  # 2nd, wt intercept = query(0) : 0.07
+  coeff<-solve(t(X) %*% X) %*% t(X) %*% data$dist
+  coeff<-round(coeff,3)
+  coeff
+  colnames(data)
+  co.ns<-grep("corpusB|cond_b|cond_c|cond_d|cond_e|cond_f",colnames(data))
+  co.df<-data.frame(coeff,row.names = c("intercept",colnames(data)[co.ns]))
+  #co.df<-data.frame(coeff,row.names = c("intercept","targetRef","b","c","d","e","f"))
+  co.df
+  p_v<-round(p_value,14)
+  
 # Reshape data: rows = q, columns = corp, values = dist
-# bar_mat <- tapply(dfe$median, list(dfe$q, dfe$target), identity)
-# bar_mat <- t(bar_mat)  # barplot expects groups in columns
-# 
-# # Make grouped barplot
-# df.plot<-barplot(bar_mat,
-#         beside = TRUE,
-#         col = c("black", "red"),
-#         names.arg = levels(dfe$q),
-#         legend.text = rownames(bar_mat),
-#         args.legend = list(x = "right"),
-#         ylab = "median distance",
-#         main = "Grouped barplot of distance by query and corp")
+bar_mat <- tapply(dfe$median, list(dfe$q, dfe$target), identity)
+bar_mat <- t(bar_mat)  # barplot expects groups in columns
+
+# Make grouped barplot
+df.plot<-barplot(bar_mat,
+        beside = TRUE,
+        col = c("black", "red"),
+        names.arg = levels(dfe$q),
+        legend.text = rownames(bar_mat),
+        args.legend = list(x = "right"),
+        ylab = "median distance",
+        main = "distance by query and corpus")
 # df.eval$mean==mdf$mean
 # df.eval$median==mdf$median
 # df.eval$mean<-mdf$mean
@@ -132,138 +203,138 @@ dfe$q <- factor(dfe$q, levels = c("a", "b", "c", "d", "e", "f"))
 #anova_model <- aov(dist ~ group + q + mf_rel , range, data = df)
 #summary(anova_model)
 #not. wks
-
-#15276.caveats, new essai
-#general test
-# build query interface
-
-build.q<-function(){
-q0<-list(a=list(q=".*",det="DET"))
-q1<-list(b=list(q=c("this","that","these","those"),det="DET")) # mean distance: 76
-q2<-list(c=list(q=c("the"),det="DET")) # mean distance: 81
-q3<-list(d=list(q=c("a","an","some","any"),det="DET")) # mean distance: 63, lower
-q4<-list(e=list(q=c("my"),det=F)) # mean distance: 55, lower
-q5<-list(f=list(q=c("your","their","his","her"),det=F)) # mean distance: 100, higher
-
-return(list(q0,q1,q2,q3,q4,q5))
-}
-qs<-build.q()
-qs[[2]]
-names(qs)
-qs
-qk<-2
-get.dist<-function(qk,tdbw,det="notset"){
-ifelse(tdbw=="obs",tdb<-tdbcorp,tdb<-tdbref)
-uid<-tdb$uid
-length(unique(uid))
-head(uid)
-uid2<-gsub("dfurl([0-9]{1,4})-.*","\\1",uid)
-#uid2<-gsub("-.*","",uid)
-head(uid2)
-length(unique(uid2))
-tdb$url<-uid2
-m<-tdb$upos=="NOUN"
-qs[[qk]][[1]]$q
-# m2<-tdbcorp$token%in%c("this","that","those","these")
-query<-unlist(qs[[qk]][[1]]$q)
-ifelse(qk!=1,m2<-tdb$token%in%query,m2<-!is.na(tdb$token)) # if condition A, match all tokens
-m1w<-which(m)
-m2w<-which(m2)
-det.q<-qs[[qk]][[1]]$det
-det.y<-F
-#det<-F
-if(mode(det)=="character")
-  det.y<-T
-ifelse(det!="notset"&det.y,det.q<-det,det.y<-F)
-# det<-F
- ifelse(det.y,1,2)
-ifelse(det.y,m5<-tdb$upos==det.q,m5<-m2)
-
-# m5<-tdb$upos=="DET"
-m5w<-which(m5)
-m6<-m2w%in%m5w
-m2w<-m2w[m6]
-sum(m6)
-# q1<-head(tdbcorp$token[(m2w)],20)
-# t1<-head(tdbcorp$token[(m2w+1)],20)
-# u1<-head(tdbcorp$upos[(m2w)],20)
-
-p1<-m1w[m2w]
-# l1<-tdbcorp$lemma[p1]
-# uid1<-tdbcorp$url[p1]
-# nouns.det<-unique(l1)
-# nouns.det<-data.frame(lemma=nouns.det,include=1)
-#write_csv(nouns.det,paste0(Sys.getenv("GIT_TOP"),"/SPUND-LX/psych/HA/nouns-002.csv"))
-
-d1<-lapply(p1, function(x){
-l2<-tdb$lemma[x]
-u2<-tdb$url[x]
-r1<-tdb$url==u2
-r1w<-which(r1)
-l3<-tdb$lemma[r1w]==l2
-l3w<-which(l3)
-d1<-diff(l3w)
-ifelse(length(d1)>0,
-  df<-data.frame(q=names(qs[[qk]]),target=tdbw,url=u2,lemma=l2,range=sum(r1),dist=d1,det=det.y),
-  df<-NA)
-#df$url<-u2
-#df$range<-length(r1w)
-return(df)
-return(data.frame(url=u2,lemma=l2,range=r1w,dist=d1))
-return(list(url=u2,lemma=l2,range=r1w,dist=d1))
-})
-
-return(d1)
-}
-qda<-get.dist(1,"obs","DET")
-qdb<-get.dist(2,"obs","DET")
-qdc<-get.dist(3,"obs","DET")
-qdd<-get.dist(4,"obs","DET")
-qde<-get.dist(5,"obs",F)
-qdf<-get.dist(6,"obs",F)
-#qdb<-get.dist(qk,"obs")
-qdar<-get.dist(1,"ref","DET")
-qdbr<-get.dist(2,"ref","DET")
-qdcr<-get.dist(3,"ref","DET")
-qddr<-get.dist(4,"ref","DET")
-qder<-get.dist(5,"ref",F)
-qdfr<-get.dist(6,"ref",F)
-#########################
-get.lt.df<-function(ql){
-  
-lt1<-ql
-#length(lt1[[1]])
-# lt2<-lapply(lt1, function(x){
-#   l<-length(x)
-#   return(ifelse(l!=0,x,NA))
+# 
+# #15276.caveats, new essai
+# #general test
+# # build query interface
+# 
+# build.q<-function(){
+# q0<-list(a=list(q=".*",det="DET"))
+# q1<-list(b=list(q=c("this","that","these","those"),det="DET")) # mean distance: 76
+# q2<-list(c=list(q=c("the"),det="DET")) # mean distance: 81
+# q3<-list(d=list(q=c("a","an","some","any"),det="DET")) # mean distance: 63, lower
+# q4<-list(e=list(q=c("my"),det=F)) # mean distance: 55, lower
+# q5<-list(f=list(q=c("your","their","his","her"),det=F)) # mean distance: 100, higher
+# 
+# return(list(q0,q1,q2,q3,q4,q5))
+# }
+# qs<-build.q()
+# qs[[2]]
+# names(qs)
+# qs
+# qk<-2
+# get.dist<-function(qk,tdbw,det="notset"){
+# ifelse(tdbw=="obs",tdb<-tdbcorp,tdb<-tdbref)
+# uid<-tdb$uid
+# length(unique(uid))
+# head(uid)
+# uid2<-gsub("dfurl([0-9]{1,4})-.*","\\1",uid)
+# #uid2<-gsub("-.*","",uid)
+# head(uid2)
+# length(unique(uid2))
+# tdb$url<-uid2
+# m<-tdb$upos=="NOUN"
+# qs[[qk]][[1]]$q
+# # m2<-tdbcorp$token%in%c("this","that","those","these")
+# query<-unlist(qs[[qk]][[1]]$q)
+# ifelse(qk!=1,m2<-tdb$token%in%query,m2<-!is.na(tdb$token)) # if condition A, match all tokens
+# m1w<-which(m)
+# m2w<-which(m2)
+# det.q<-qs[[qk]][[1]]$det
+# det.y<-F
+# #det<-F
+# if(mode(det)=="character")
+#   det.y<-T
+# ifelse(det!="notset"&det.y,det.q<-det,det.y<-F)
+# # det<-F
+#  ifelse(det.y,1,2)
+# ifelse(det.y,m5<-tdb$upos==det.q,m5<-m2)
+# 
+# # m5<-tdb$upos=="DET"
+# m5w<-which(m5)
+# m6<-m2w%in%m5w
+# m2w<-m2w[m6]
+# sum(m6)
+# # q1<-head(tdbcorp$token[(m2w)],20)
+# # t1<-head(tdbcorp$token[(m2w+1)],20)
+# # u1<-head(tdbcorp$upos[(m2w)],20)
+# 
+# p1<-m1w[m2w]
+# # l1<-tdbcorp$lemma[p1]
+# # uid1<-tdbcorp$url[p1]
+# # nouns.det<-unique(l1)
+# # nouns.det<-data.frame(lemma=nouns.det,include=1)
+# #write_csv(nouns.det,paste0(Sys.getenv("GIT_TOP"),"/SPUND-LX/psych/HA/nouns-002.csv"))
+# 
+# d1<-lapply(p1, function(x){
+# l2<-tdb$lemma[x]
+# u2<-tdb$url[x]
+# r1<-tdb$url==u2
+# r1w<-which(r1)
+# l3<-tdb$lemma[r1w]==l2
+# l3w<-which(l3)
+# d1<-diff(l3w)
+# ifelse(length(d1)>0,
+#   df<-data.frame(q=names(qs[[qk]]),target=tdbw,url=u2,lemma=l2,range=sum(r1),dist=d1,det=det.y),
+#   df<-NA)
+# #df$url<-u2
+# #df$range<-length(r1w)
+# return(df)
+# return(data.frame(url=u2,lemma=l2,range=r1w,dist=d1))
+# return(list(url=u2,lemma=l2,range=r1w,dist=d1))
 # })
-lt2<-lt1[!is.na(lt1)]
-lt3<-lapply(lt2, function(x){
-  df3<-data.frame(x$dist)
-  df3$q<-x$q
-  df3$target<-x$target
-  df3$url<-x$url
-  df3$lemma<-x$lemma
-  df3$range<-x$range
-  df3$det<-x$det
-#  df3$mf_rel<-x[[1]]$mf_rel
- # df3$ld<-x[[1]]$ld
-  return(df3)
-  return(x[[1]]$dist)
-})
-library(abind)
-lt4.df<-data.frame(abind(lt3,along = 1))
-colnames(lt4.df)[1]<-"dist"
-mode(lt4.df$dist)<-"double"
-mode(lt4.df$range)<-"double"
-print(mean(lt4.df$dist))
-print(median(lt4.df$dist))
-return(lt4.df)
-}
-
-qltdf<-rbind(get.lt.df(qda),get.lt.df(qdb),get.lt.df(qdc),get.lt.df(qdd),get.lt.df(qde),get.lt.df(qdf),
-             get.lt.df(qdar),get.lt.df(qdbr),get.lt.df(qdcr),get.lt.df(qddr),get.lt.df(qder),get.lt.df(qdfr))
-#write.csv(qltdf,paste0(Sys.getenv("GIT_TOP"),"/SPUND-LX/psych/HA/eval-003.csv"))
+# 
+# return(d1)
+# }
+# qda<-get.dist(1,"obs","DET")
+# qdb<-get.dist(2,"obs","DET")
+# qdc<-get.dist(3,"obs","DET")
+# qdd<-get.dist(4,"obs","DET")
+# qde<-get.dist(5,"obs",F)
+# qdf<-get.dist(6,"obs",F)
+# #qdb<-get.dist(qk,"obs")
+# qdar<-get.dist(1,"ref","DET")
+# qdbr<-get.dist(2,"ref","DET")
+# qdcr<-get.dist(3,"ref","DET")
+# qddr<-get.dist(4,"ref","DET")
+# qder<-get.dist(5,"ref",F)
+# qdfr<-get.dist(6,"ref",F)
+# #########################
+# get.lt.df<-function(ql){
+#   
+# lt1<-ql
+# #length(lt1[[1]])
+# # lt2<-lapply(lt1, function(x){
+# #   l<-length(x)
+# #   return(ifelse(l!=0,x,NA))
+# # })
+# lt2<-lt1[!is.na(lt1)]
+# lt3<-lapply(lt2, function(x){
+#   df3<-data.frame(x$dist)
+#   df3$q<-x$q
+#   df3$target<-x$target
+#   df3$url<-x$url
+#   df3$lemma<-x$lemma
+#   df3$range<-x$range
+#   df3$det<-x$det
+# #  df3$mf_rel<-x[[1]]$mf_rel
+#  # df3$ld<-x[[1]]$ld
+#   return(df3)
+#   return(x[[1]]$dist)
+# })
+# library(abind)
+# lt4.df<-data.frame(abind(lt3,along = 1))
+# colnames(lt4.df)[1]<-"dist"
+# mode(lt4.df$dist)<-"double"
+# mode(lt4.df$range)<-"double"
+# print(mean(lt4.df$dist))
+# print(median(lt4.df$dist))
+# return(lt4.df)
+# }
+# 
+# qltdf<-rbind(get.lt.df(qda),get.lt.df(qdb),get.lt.df(qdc),get.lt.df(qdd),get.lt.df(qde),get.lt.df(qdf),
+#              get.lt.df(qdar),get.lt.df(qdbr),get.lt.df(qdcr),get.lt.df(qddr),get.lt.df(qder),get.lt.df(qdfr))
+# #write.csv(qltdf,paste0(Sys.getenv("GIT_TOP"),"/SPUND-LX/psych/HA/eval-003.csv"))
 
 
 paste(q1,t1,u1,sep = " ")
@@ -289,3 +360,4 @@ print(tdb$token[r1])
 print(tdb$upos[r1])
 }
 show.tok(2)
+}
--- psych/HA/ref.distancer.r
+++ psych/HA/ref.distancer.r
@@ -12,6 +12,7 @@ tdbcorp<-dbGetQuery(con,"SELECT * FROM reddit_com_pos")
 #tdb<-tdbcorp
 #tdb.com<-dbGetQuery(con,"SELECT * FROM redditpsych")
 ###
+dep.fun<-function(){
 # task: devise referrer distance of demonstratives
 # build query interface
 q1<-list(token=c("this","that","these","those")) # mean distance: 76
@@ -658,7 +659,7 @@ data<-dfa
  t2<-diff(t1)
 # t3<-
 data$range_c    <- data$range    - mean(data$range[data$q=="a"]) # level intercept for conditions b-f 
-data$range_c    <- data$range    - mean(data$range[data$q=="a"]) # level intercept for conditions b-f 
+#data$range_c    <- data$range    - mean(data$range[data$q=="a"]) # level intercept for conditions b-f 
 #data$corpsize_c <- data$corp_size - mean(data$corp_size)
 #data$m_rel_c    <- data$m_rel    - mean(data$m_rel)
 #data$m_rel_c    <- data$mf_rel - mean(data$mf_rel[data$q=="a"])
@@ -742,10 +743,12 @@ p_value <- 2 * pt(-abs(t_value), df)
 coeff<-solve(t(X) %*% X) %*% t(X) %*% data$dist
 coeff<-round(coeff,3)
 coeff
-co.df<-data.frame(coeff,row.names = c("intercept",colnames(data)[c(11,3,7,13,14,15,16,17)]))
-co.df<-data.frame(coeff,row.names = c("intercept","targetRef","b","c","d","e","f"))
+colnames(data)
+co.ns<-grep("corpusB|cond_b|cond_c|cond_d|cond_e|cond_f",colnames(data))
+co.df<-data.frame(coeff,row.names = c("intercept",colnames(data)[co.ns]))
+#co.df<-data.frame(coeff,row.names = c("intercept","targetRef","b","c","d","e","f"))
 co.df
-p_v<-round(p_value,5)
+p_v<-round(p_value,14)
 # # Fit reduced model without the term of interest
 # reduced_model <- lmer(d ~ q + (1|corp_size), data = df) 
 # full_model <- lmer(d ~ corp*m + (1|corp_size), data = df)
@@ -820,14 +823,149 @@ sum(m1)
 nouns<-unique(tdbcorp$lemma[m1])
 nouns.df<-data.frame(lemma=nouns,include=1)
 #write_csv(nouns.df,paste0(Sys.getenv("GIT_TOP"),"/SPUND-LX/psych/HA/nouns-001.csv"))
+}
 
+#15276.caveats, new essai
+#general test
+get.dist.df<-function(){
 
+# build query interface
+build.q<-function(){
+  q0<-list(a=list(q=".*",det="DET"))
+  q1<-list(b=list(q=c("this","that","these","those"),det="DET")) # mean distance: 76
+  q2<-list(c=list(q=c("the"),det="DET")) # mean distance: 81
+  q3<-list(d=list(q=c("a","an","some","any"),det="DET")) # mean distance: 63, lower
+  q4<-list(e=list(q=c("my"),det=F)) # mean distance: 55, lower
+  q5<-list(f=list(q=c("your","their","his","her"),det=F)) # mean distance: 100, higher
+  
+  return(list(q0,q1,q2,q3,q4,q5))
+}
+qs<-build.q()
+qs[[2]]
+names(qs)
+qs
+qk<-2
+get.dist<-function(qk,tdbw,det="notset"){
+  ifelse(tdbw=="obs",tdb<-tdbcorp,tdb<-tdbref)
+  uid<-tdb$uid
+  length(unique(uid))
+  head(uid)
+  uid2<-gsub("dfurl([0-9]{1,4})-.*","\\1",uid)
+  #uid2<-gsub("-.*","",uid)
+  head(uid2)
+  length(unique(uid2))
+  tdb$url<-uid2
+  m<-tdb$upos=="NOUN"
+  qs[[qk]][[1]]$q
+  # m2<-tdbcorp$token%in%c("this","that","those","these")
+  query<-unlist(qs[[qk]][[1]]$q)
+  ifelse(qk!=1,m2<-tdb$token%in%query,m2<-!is.na(tdb$token)) # if condition A, match all tokens
+  m1w<-which(m)
+  m2w<-which(m2)
+  det.q<-qs[[qk]][[1]]$det
+  det.y<-F
+  #det<-F
+  if(mode(det)=="character")
+    det.y<-T
+  ifelse(det!="notset"&det.y,det.q<-det,det.y<-F)
+  # det<-F
+  ifelse(det.y,1,2)
+  ifelse(det.y,m5<-tdb$upos==det.q,m5<-m2)
+  
+  # m5<-tdb$upos=="DET"
+  m5w<-which(m5)
+  m6<-m2w%in%m5w
+  m2w<-m2w[m6]
+  sum(m6)
+  # q1<-head(tdbcorp$token[(m2w)],20)
+  # t1<-head(tdbcorp$token[(m2w+1)],20)
+  # u1<-head(tdbcorp$upos[(m2w)],20)
+  
+  p1<-m1w[m2w]
+  # l1<-tdbcorp$lemma[p1]
+  # uid1<-tdbcorp$url[p1]
+  # nouns.det<-unique(l1)
+  # nouns.det<-data.frame(lemma=nouns.det,include=1)
+  #write_csv(nouns.det,paste0(Sys.getenv("GIT_TOP"),"/SPUND-LX/psych/HA/nouns-002.csv"))
+  
+  d1<-lapply(p1, function(x){
+    l2<-tdb$lemma[x]
+    u2<-tdb$url[x]
+    r1<-tdb$url==u2
+    r1w<-which(r1)
+    l3<-tdb$lemma[r1w]==l2
+    l3w<-which(l3)
+    d1<-diff(l3w)
+    ifelse(length(d1)>0,
+           df<-data.frame(q=names(qs[[qk]]),target=tdbw,url=u2,lemma=l2,range=sum(r1),dist=d1,det=det.y),
+           df<-NA)
+    #df$url<-u2
+    #df$range<-length(r1w)
+    return(df)
+    return(data.frame(url=u2,lemma=l2,range=r1w,dist=d1))
+    return(list(url=u2,lemma=l2,range=r1w,dist=d1))
+  })
+  
+  return(d1)
+}
+qda<-get.dist(1,"obs","DET")
+qdb<-get.dist(2,"obs","DET")
+qdc<-get.dist(3,"obs","DET")
+qdd<-get.dist(4,"obs","DET")
+qde<-get.dist(5,"obs",F)
+qdf<-get.dist(6,"obs",F)
+#qdb<-get.dist(qk,"obs")
+qdar<-get.dist(1,"ref","DET")
+qdbr<-get.dist(2,"ref","DET")
+qdcr<-get.dist(3,"ref","DET")
+qddr<-get.dist(4,"ref","DET")
+qder<-get.dist(5,"ref",F)
+qdfr<-get.dist(6,"ref",F)
+#########################
+get.lt.df<-function(ql){
+  
+  lt1<-ql
+  #length(lt1[[1]])
+  # lt2<-lapply(lt1, function(x){
+  #   l<-length(x)
+  #   return(ifelse(l!=0,x,NA))
+  # })
+  lt2<-lt1[!is.na(lt1)]
+  lt3<-lapply(lt2, function(x){
+    df3<-data.frame(x$dist)
+    df3$q<-x$q
+    df3$target<-x$target
+    df3$url<-x$url
+    df3$lemma<-x$lemma
+    df3$range<-x$range
+    df3$det<-x$det
+    #  df3$mf_rel<-x[[1]]$mf_rel
+    # df3$ld<-x[[1]]$ld
+    return(df3)
+    return(x[[1]]$dist)
+  })
+  library(abind)
+  lt4.df<-data.frame(abind(lt3,along = 1))
+  colnames(lt4.df)[1]<-"dist"
+  mode(lt4.df$dist)<-"double"
+  mode(lt4.df$range)<-"double"
+  print(mean(lt4.df$dist))
+  print(median(lt4.df$dist))
+  return(lt4.df)
+}
+
+qltdf<-rbind(get.lt.df(qda),get.lt.df(qdb),get.lt.df(qdc),get.lt.df(qdd),get.lt.df(qde),get.lt.df(qdf),
+             get.lt.df(qdar),get.lt.df(qdbr),get.lt.df(qdcr),get.lt.df(qddr),get.lt.df(qder),get.lt.df(qdfr))
+write.csv(qltdf,paste0(Sys.getenv("GIT_TOP"),"/SPUND-LX/psych/HA/eval-003.csv"))
+
+}
+#get.dist.df()
 
 
 # write_csv(qdf,"~/gith/SPUND-LX/psych/HA/eval-001.csv")
 library(readr)
- write_csv(df1,paste0(Sys.getenv("GIT_TOP"),"/SPUND-LX/psych/HA/eval-001.csv"))
- write_csv(q.all.df,paste0(Sys.getenv("GIT_TOP"),"/SPUND-LX/psych/HA/eval-002.csv"))
+ # write_csv(df1,paste0(Sys.getenv("GIT_TOP"),"/SPUND-LX/psych/HA/eval-001.csv"))
+ # write_csv(q.all.df,paste0(Sys.getenv("GIT_TOP"),"/SPUND-LX/psych/HA/eval-002.csv"))
  # #  library(jsonlite)
 # write_json(list(a=q0,b=q1,c=q2,d=q3,e=q4,f=q5),paste0(Sys.getenv("GIT_TOP"),"/SPUND-LX/psych/HA/eval-qs.json"))
 
