d08ce2391d2bd4f70632a29eb6551f0f32bab6f2
st. <91368979+esteeschwarz@users.noreply.github.com>
2025-11-03 09:41:50 +0100

    04

--- germanic/app/germanic-remote/app-rem.R
+++ germanic/app/germanic-remote/app-rem.R
@@ -91,7 +91,7 @@ fluidPage(
     DTOutput("showsamples"),
     hr(),
     #textInput("id","input row ID to show complete KWIC sample"),
-    verbatimTextOutput("info","click at row to see KWIC sample"),
+    HTML("<p>click at row to see KWIC sample</p>"),
     tableOutput("row")
     
     
