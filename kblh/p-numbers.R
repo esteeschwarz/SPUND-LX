6fea77249c6822d85f8d4ad5d8d04eebc8c8ce21
esteeschwarz <91368979+esteeschwarz@users.noreply.github.com>
2025-11-25 20:41:25 +0100

    kblh p-numbers

--- kblh/p-numbers.R
+++ kblh/p-numbers.R
@@ -12,13 +12,15 @@
 #x<-6
 #k<-1
 xl<-1:10000
+# 6,28,496,8128
+xl-10001:15000
 #xl<-list(1:2000)
 pa<-array()
 #xlist<-list(1:10000)
 
 xk<-lapply(xl,function(i){
   cat("\r",i)
-  p<-i-1
+  #p<-i-1
   for (k in 1:i){
 #    cat("\r",k)
     b<-i/k
@@ -28,8 +30,9 @@ xk<-lapply(xl,function(i){
   #pa<-pa[!is.na(pa)]
   m<-pa==i
   pa<-pa[!m]
-  pp<-sum(pa)==x
+ # pp<-sum(pa)==x
   pp<-sum(pa,na.rm = T)==i
+  
   return(pp)
 })
 sum(unlist(xk))
