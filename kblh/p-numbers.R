bb5df751e8d915e0bbbe9692e7bdf2897fc9ff0e
esteeschwarz <91368979+esteeschwarz@users.noreply.github.com>
2025-11-28 06:16:02 +0100

    upd 2x lapply instead loop. 1:100000 took ca. 10h on srv

--- kblh/p-numbers.R
+++ kblh/p-numbers.R
@@ -11,8 +11,9 @@
 
 #x<-6
 #k<-1
-#n<-1000
-#s<-1
+n<-1000
+s<-1
+i<-100
 get.pn<-function(s,n,m,csv){
 xl<-s:n
 # 6,28,496,8128
@@ -27,15 +28,22 @@ write.table(df,csv,quote = F)
 xk<-lapply(xl,function(i){
   cat("\r",i)
   #p<-i-1
-  for (k in 1:i){
-#    cat("\r",k)
-    b<-i/k
-#    ifelse(ifelse(m=="c",ceiling(b)==b,typeof(b)=="int"),pa[k]<-b,pa[k]<-0) # bench time: 2.225
-    #ifelse(typeof(b)=="int",pa[k]<-b,pa[k]<-0) # bench time: 1.127 
-    ifelse(ceiling(b)==b,pa[k]<-b,pa[k]<-0) # bench time: 1.095
-    
-  }
+#   for (k in 1:i){
+# #    cat("\r",k)
+#     b<-i/k
+# #    ifelse(ifelse(m=="c",ceiling(b)==b,typeof(b)=="int"),pa[k]<-b,pa[k]<-0) # bench time: 2.225
+#     #ifelse(typeof(b)=="int",pa[k]<-b,pa[k]<-0) # bench time: 1.127 
+#     ifelse(ceiling(b)==b,pa[k]<-b,pa[k]<-0) # bench time: 1.095
+#     
+#   }
+  x2<-1:i
+  pa<-lapply(x2, function(ii){
+    #print(ii)
+    b<-i/ii
+    ifelse(ceiling(b)==b,b,0) # bench time: 1.095
+  })
   #pa<-pa[!is.na(pa)]
+  pa<-unlist(pa)
   m<-pa==i
   pa<-pa[!m]
  # pp<-sum(pa)==x
@@ -43,18 +51,21 @@ xk<-lapply(xl,function(i){
   df<-data.frame(start=s,lim=n,p.number=i)
   if(pp)
     write.table(df,csv,append = T,quote = F,col.names = F)
+  return(pa)
   return(pp)
 })
-sum(unlist(xk))
-xk2<-unlist(xk)
-pn<-which(xk2)
+xs<-lapply(xk, sum)
+xo<-which(xs==s:n)
+#sum(unlist(xk))
+#xk2<-unlist(xk)
+#pn<-which(xk2)
 cat("\np-number range 1 to",n,"\n")
-print(pn)
+print(xo)
 
 }
 #csv<-paste0(Sys.getenv("GIT_TOP"),"/SPUND-LX/kblh/p-numbers.csv")
 csv<-"/var/www/html/cloud/p-numbers.csv"
-dur<-system.time(get.pn(1,100,"c",csv)) #v0.1: 1:1000 1.208
+dur<-system.time(get.pn(100001,150000,"c",csv)) #v0.1: 1:1000 1.208
 dur
 write.table(data.frame(time=Sys.time(),elapsed=dur[3],row.names = "finished"),csv,append = T,quote = F,col.names = T)
 
