fad8a330f5a249f014e4ee36ab3af2b5a688e8ca
estee-lapsi <91368979+esteeschwarz@users.noreply.github.com>
2025-11-30 15:24:30 +0100

    use page.ids instead page.id

--- public/openlx-template/create-yml.R
+++ public/openlx-template/create-yml.R
@@ -22,12 +22,12 @@ p
 q<-paste0(paste0(rep("..",p),collapse = "/"),"/q")
 q
 list.dirs(q)
-q.dir<-paste0(q,"/",head.index.qmd$id)
+q.dir<-paste0(q,"/",head.index.qmd$ids)
 q.dir
 #d1<-list.dirs("../../.."),d1)
 quarto.yml$project$`output-dir`<-q.dir
 quarto.yml$website$title<-head.index.qmd$site_title
-quarto.yml$website$navbar$left[[1]]$href<-paste0("https://esteeschwarz.github.io/open-lx/posts/",head.index.qmd$id)
+quarto.yml$website$navbar$left[[1]]$href<-paste0("https://esteeschwarz.github.io/open-lx/posts/",head.index.qmd$ids)
 quarto.yml$website$navbar$right[[2]]$href<-paste0("https://github.com/esteeschwarz/SPUND-LX/tree/main/",head.index.qmd$project_dir)
 ## post.md
 #head.post.md$site_link<-paste0("[",head.index.qmd$site_link_text,"](../../essais/",head.index.qmd$id,")")
@@ -40,7 +40,7 @@ head.post.md$title<-head.index.qmd$title
 head.post.md$subtitle<-head.index.qmd$subtitle
 head.post.md$class<-head.index.qmd$class
 head.post.md$task<-head.index.qmd$task
-head.post.md$id<-head.index.qmd$id
+head.post.md$ids<-head.index.qmd$ids
 head.post.md$description<-head.index.qmd$description
 
 handlers <- list(
@@ -67,15 +67,15 @@ yaml_body(head.post.md)
 post.md<-c("---",post.tx,"---")
 post.md
 #writeLines(post.md,paste0("cp/",head.post.md$date,"-",head.index.qmd$id,".md"))
-writeLines(post.md,paste0(post.ns.dir,"/",head.post.md$date,"-",head.index.qmd$id,".md"))
+writeLines(post.md,paste0(post.ns.dir,"/",head.post.md$date,"-",head.index.qmd$ids,".md"))
 ## workflow.yml
 workflow.yml$`on`$push$paths<-paste0(head.index.qmd$input_dir,"/**")
 workflow.yml$jobs$render$steps[2][[1]]
 workflow.yml$jobs$render$steps[2][[1]]$run<-paste0("quarto render ",head.index.qmd$input_dir," -P reload:false\nls q")
-workflow.yml$name<-paste0("Render quarto (",head.index.qmd$id,")")
-workflow.yml$jobs$deploy$steps[[4]]$run<-gsub("SMI",head.index.qmd$id,workflow.yml$jobs$deploy$steps[[4]]$run)
-write_yaml(workflow.yml,paste0(workflow.ns,"/quarto-",head.index.qmd$id,".yml"))
-write_yaml(workflow.yml,paste0(Sys.getenv("GIT_TOP"),"/SPUND-LX/public/openlx-template/cp/quarto-",head.index.qmd$id,".yml"),handlers=handlers)
+workflow.yml$name<-paste0("Render quarto (",head.index.qmd$ids,")")
+workflow.yml$jobs$deploy$steps[[4]]$run<-gsub("SMI",head.index.qmd$ids,workflow.yml$jobs$deploy$steps[[4]]$run)
+write_yaml(workflow.yml,paste0(workflow.ns,"/quarto-",head.index.qmd$ids,".yml"))
+write_yaml(workflow.yml,paste0(Sys.getenv("GIT_TOP"),"/SPUND-LX/public/openlx-template/cp/quarto-",head.index.qmd$ids,".yml"),handlers=handlers)
 write_yaml(quarto.yml,"_quarto.yml",handlers=handlers)
 filename <- tempfile()
 con <- file("_quarto.yml", "w")
