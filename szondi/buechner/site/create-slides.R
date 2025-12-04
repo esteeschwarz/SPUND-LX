1b8a8d403f7b2e1389fd81da548f651d01ab8f94
estee-lapsi <91368979+esteeschwarz@users.noreply.github.com>
2025-12-04 17:41:47 +0100

    upd slides

--- szondi/buechner/site/create-slides.R
+++ szondi/buechner/site/create-slides.R
@@ -1,12 +1,27 @@
 t<-readLines("qa.qmd")
-m<-grep("2.",t)
-t2<-t[m:length(t)]
-a<-strsplit(t[m:length(t)],"[0-9]+\\. ")
-a2<-lapply(a, function(x){
-  c("## ",x[2],"")
-})
-a2<-lapply(t2, function(x){
-  c("## ",x,"")
+t<-readLines("notes.raw.md")
+t<-t[t!=""]
+t<-t[!grepl("reception penthes",t)]
+#m<-grep("2.",t)[1]
+#t2<-t[m:length(t)]
+#a<-strsplit(t[m:length(t)],"[0-9]+\\. ")
+#a<-strsplit(t,"[0-9]+\\. ")
+#strsplit(t[1],"[0-9]+\\.")
+library(stringi)
+stri_match(t,regex="([0-9]+)\\. (.+)( \\\\>\\\\>)")
+t2<-t
+# a2<-lapply(a, function(x){
+#   c("## ",x[2],"")
+# })
+a2<-lapply(seq_along(t2), function(x){
+  
+  t<-t2[x]
+  a<-stri_match(t,regex="([0-9]+)\\. (.+)( \\\\>\\\\>)")
+  n<-a[2]
+  t<-a[3]
+#  a<-strsplit(t,"[0-9]+\\. ")
+  n<-paste0("## ",n)
+  c(n,t,"")
 })
 a2<-unlist(a2)
 a3<-readLines("_slides.qmd")
