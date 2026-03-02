5661781a205d662b833ed2a651e12ae8bfd91a3d
esteeschwarz <91368979+esteeschwarz@users.noreply.github.com>
2026-03-02 16:29:29 +0100

    try grid layout, not wks, todo. restore old.

---
+++ germanic/HA/poster/testqmd.R
@@ -0,0 +1,10 @@
+# testqmd
+setwd("Documents/GitHub/SPUND-LX/germanic/HA/poster")
+load("../drafts/plist.RData")
+list.files()
+plist$sdf
+plist$ptx
+prompt<-plist$ptx
+prompt<-paste(prompt,collapse = "\n")
+prompt
+plist$boxdesc
