413e541d73e8219df2e6c3d353c4b1fae26797d1
estee-lapsi <91368979+esteeschwarz@users.noreply.github.com>
2026-04-17 17:25:02 +0200

    wiener annotations

--- play/quarto/start/prerender.R
+++ play/quarto/start/prerender.R
@@ -1,3 +1,6 @@
 # check system, adapt output
 s<-Sys.getenv("SYS")
 outputdir<-ifelse (s=="mini12","/var/www/html/play/pages","../output/pages/004")
+
+source("fetch-zotero.R")
+source("annotations.R")
