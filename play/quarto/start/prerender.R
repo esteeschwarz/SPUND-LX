42ad08d8f128f6bbf50e9d13e289d2756bdc2b89
estee-lapsi <91368979+esteeschwarz@users.noreply.github.com>
2026-04-20 07:20:18 +0200

    upd annotations from db, wks.

--- play/quarto/start/prerender.R
+++ play/quarto/start/prerender.R
@@ -1,15 +1,17 @@
 # check system, adapt output
 s<-Sys.getenv("SYS")
 buildmdb<-function(){
-  qa<-c(litKI="Wiener_Einführung.pdf",nietzsche=c("nietzsche, kga 3-1, geburt d tragödie.pdf","nietzsche briefe ggl.pdf"),textur=NA) # margin note studyset
-  source("getnotesql.R")
+qa<-list(litKI="Wiener_Einführung.pdf",
+nietzsche=c("nietzsche, kga 3-1, geburt d tragödie.pdf","nietzsche briefe ggl.pdf"),
+textur=c("2 - Behmenburg, Lena - Das Weben von Texten und Texturen.pdf","langlois-2019-distributed-intelligence-silk-weaving-and-the-jacquard-mechanism.pdf","07_Munk+Rosing.pdf")) # margin note studyset  
+source("getnotesql.R")
 }
 outputdir<-ifelse (s=="mini12","/var/www/html/play/pages","../output/pages/004")
 ifelse (s%in%c("lapsi","tapee"),buildmdb(),F)
 
 ### vars
 docdir<-"."
-qa<-c(litKI="wiener_kybernetik_1992",nietzsche=c("nietzsche_kga_2024","nietzsche_friedrich_1902"),textur=NA) # margin note studyset
+qa<-c(litKI="wiener_kybernetik_1992",nietzsche=c("nietzsche_kga_2024"),textur=NA) # margin note studyset
 
 
 qa
@@ -18,4 +20,4 @@ bibyml<-qa
 ########
 qa
 source("fetch-zotero.R")
-source("annotations.R")
+#source("annotations.R")
