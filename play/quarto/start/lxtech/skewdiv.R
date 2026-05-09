93659534c82302577012fcc26b9cf1080dacf596
estee-lapsi <91368979+esteeschwarz@users.noreply.github.com>
2026-05-09 05:49:01 +0200

    löwenzahn test row

--- play/quarto/start/lxtech/skewdiv.R
+++ play/quarto/start/lxtech/skewdiv.R
@@ -8,15 +8,20 @@
 # Q: zinsmeister(2013)
 
 
-# D(p||q) = Si pi x log(pi/qi)
+# D(p||q) = Sig^i p^i x log(p^i/q^i)
 l1<-c(0.6,0.4,0) # milchzahn
 l2<-c(0.25,0.5,0.25) # zahn
 l3<-c(0,0,1) # löwenzahn
+### test: löwenzahn::ziehen=1 occurence instead 0
+l4<-c(0,1,2)
+l5<-unlist(lapply(l4,function(x){
+x/sum(l4)
+}))
 ########################
 # l2 is the reference p
 # l1,l3 are the target q probabilities which are projected on /smoothed by reference p
 #############################################
-lx<-list(list(l2,l1),list(l2,l3))
+lx<-list(list(l2,l1),list(l2,l3),list(l2,l5))
 #f<-sum(pi*(log(pi/qi)))
 w<-0.9
 s1<-lapply(lx,function(l){
@@ -31,9 +36,13 @@ s1<-lapply(lx,function(l){
       cat("qi:",qi,"\n")
       # s[i]<-sum(pi*log((pi/qi)))
       s[i]<-p1*log(p1/qi)
-      cat("s:",s[1],"\n")
+      cat("s:",s[i],"\n")
     }
     spq<-sum(s)
-    })
-
-s1 #chk.
+    cat("skew div:",spq,"\n")
+  return(s)
+})
+names(s1)<-c("milchzahn","löwenzahnA","löwenzahnB")
+lapply(s1,sum) #chk.
+par(las=2)
+boxplot(s1,main="skew divergence scores, reference: zahn")
