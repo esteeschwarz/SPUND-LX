dab98f853f10fc7eeba5ad30decd4fc40557def1
estee-lapsi <91368979+esteeschwarz@users.noreply.github.com>
2026-06-21 10:53:11 +0200

    update nietzsche, textur

--- play/quarto/start/lxtech/perm-reset.sh
+++ play/quarto/start/lxtech/perm-reset.sh
@@ -3,10 +3,10 @@ tccutil reset All co.posit.positron
 # ls '/Users/guhl/Library/Mobile Documents/iCloud~QReader~MarginStudy~easy/Documents/MN3/A_UNI/SZONDI/textur'
 # git update-index --refresh
 # git status
-md5 margindb.RData
-md5 ~/db/margindbnew.RData
-ls -la margindb.RData
-md5 "margindb copy.RData"
+# md5 margindb.RData
+# md5 ~/db/margindbnew.RData
+# ls -la margindb.RData
+# md5 "margindb copy.RData"
 
 export BASEd='/Users/guhl/Library/Mobile Documents/iCloud~md~obsidian/Documents/lapsi/docs/MN4/OBS/A_UNI'
 echo "margin docs > $BASEd"
