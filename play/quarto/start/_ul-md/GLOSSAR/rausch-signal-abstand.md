3837c2189207330f1aa87da70568575d4766d957
st. <91368979+esteeschwarz@users.noreply.github.com>
2026-06-22 09:27:18 +0200

    upd obs tapi

--- play/quarto/start/_ul-md/GLOSSAR/rausch-signal-abstand.md
+++
@@ -1,1 +0,0 @@
-# rausch - signal - abstand
--- play/quarto/start/lxtech/scribble.sh
+++ play/quarto/start/lxtech/scribble.sh
@@ -1,8 +1,8 @@
 #osascript -e 'id of app "Positron"'
 tccutil reset All co.posit.positron
-# ls '/Users/guhl/Library/Mobile Documents/iCloud~QReader~MarginStudy~easy/Documents/MN3/A_UNI/SZONDI/textur'
-# git update-index --refresh
-# git status
+ls '/Users/guhl/Library/Mobile Documents/iCloud~QReader~MarginStudy~easy/Documents/MN3/A_UNI/SZONDI/textur'
+git update-index --refresh
+git status
 md5 margindb.RData
 md5 ~/db/margindbnew.RData
 ls -la margindb.RData
