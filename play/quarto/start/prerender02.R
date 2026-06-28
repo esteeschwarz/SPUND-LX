34947a0ca3bf1fb12e813299b1986ed6b018b9c2
estee-lapsi <91368979+esteeschwarz@users.noreply.github.com>
2026-06-28 14:48:42 +0200

    upd db from new essai

--- play/quarto/start/prerender02.R
+++ play/quarto/start/prerender02.R
@@ -72,16 +72,18 @@ smdb<-function(qa){
 # textur=c("2 - Behmenburg, Lena - Das Weben von Texten und Texturen.pdf","langlois-2019-distributed-intelligence-silk-weaving-and-the-jacquard-mechanism.pdf","07_Munk+Rosing.pdf")) # margin note studyset
 source(paste0(Sys.getenv("GIT_TOP"),"/R-essais/scripts/marginnotesdb.R"))
 #margindb<-get.notes(qa)
-margin1<-get.margin(qa)  
-qa
-dbsub<-margin1
+margincp<-get.margin(qa)
+margin1<-margincp$dbsub  
+qa<-margincp$qa
+dbsub<-margincp$dbsub
 #dbsub<-margindb[margindb$doc%in%unlist(qa),]
 names(qa)
 qa
 #dbsub<-margindb[margindb$doc%in%names(qa),]
-margin_sf<-margin1
+margin_sf<-margincp
 margindb<-list(qa=qa,dbsub=dbsub)
 dbnew<-margindb
+getwd()
 load("margindb.RData")
 fi<-file.info("margindb.RData")
 #fi<-file.info("margin1.RData")
